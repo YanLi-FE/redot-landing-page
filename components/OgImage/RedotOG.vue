ab7f9d3b92c169baf320b01cc7a779602376cb2a
Victor Chanil Park <opdev1004@gmail.com>
2024-10-11 03:29:17 +0900

    feat: meta tags and basic setting of og image generation

---
+++ components/OgImage/RedotOG.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    title?: string;
+    description?: string;
+    imageUrl?: string;
+    backgroundColor?: string;
+    brandColor?: string;
+  }>(),
+  {
+    title: "title",
+    description: "description",
+    imageUrl: "",
+    backgroundColor: "#000000",
+    brandColor: "#FF6E01",
+  }
+);
+</script>
+
+<template>
+  <div>
+    <div>
+      <div>
+        <h1>
+          {{ title }}
+        </h1>
+        <h2>
+          {{ description }}
+        </h2>
+        <img v-if="imageUrl" alt="" :src="imageUrl" />
+        <p>redotengine.org</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,3 +1,29 @@
+<script setup lang="ts">
+import { useRoute } from "#app";
+const route = useRoute();
+
+const { data: post } = await useAsyncData("post", () =>
+  queryContent()
+    .where({ _path: `/blog/${route.params.slug}` })
+    .findOne()
+);
+
+defineOgImageComponent("RedotOG", {
+  title: post.value?.title,
+  description: post.value?.description,
+  imageUrl: "",
+  backgroundColor: "#000000",
+  brandColor: "#FF6E01",
+});
+
+useSeoMeta({
+  title: post.value?.title,
+  ogTitle: post.value?.title,
+  description: post.value?.description,
+  ogDescription: post.value?.description,
+});
+</script>
+
 <template>
   <div>
     <ContentDoc />
